5af3bd8967ab385f9499ae76d0533fb8993477c1
cristian@onlab.uy <cristian@onlab.uy>
2022-08-17 09:54:52 -0300

    Primer commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect } from "react";
+import { tasks as data } from "../data/tasks";
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: new Date().getMilliseconds().toString(),
+        description: task.description,
+      },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((x) => x.id !== taskId));
+  }
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        deleteTask,
+        createTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
---
+++ src/data/tasks.js
@@ -0,0 +1,20 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'mi primer tarea',
+        description: 'mi primera tarea descripción'
+
+    },
+    {
+        id: 1,
+        title: 'mi segunda tarea ',
+        description: 'mi segunda tarea descripción'
+
+    },
+    {
+        id: 2,
+        title: 'mi tercer tarea ',
+        description: 'mi tercer tarea descripción'
+
+    },
+];
